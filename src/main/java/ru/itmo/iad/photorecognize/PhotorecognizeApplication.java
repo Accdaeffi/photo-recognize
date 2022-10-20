741d9efa5bcb14ff7365b0ae803c7cb1f8fdbb83
Дмитрий Носков <noskoff2@yandex.ru>
2022-10-20 22:14:59 +0300

    Initial commit

---
+++ src/main/java/ru/itmo/iad/photorecognize/PhotorecognizeApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.iad.photorecognize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotorecognizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotorecognizeApplication.class, args);
+	}
+
+}
