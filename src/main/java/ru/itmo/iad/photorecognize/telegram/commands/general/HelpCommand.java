4a64e17fe71be8aae4a337b3fd807bc4c7be8715
iburakov <speedwatson@gmail.com>
2022-10-26 20:12:36 +0300

    Update links

--- src/main/java/ru/itmo/iad/photorecognize/telegram/commands/general/HelpCommand.java
+++ src/main/java/ru/itmo/iad/photorecognize/telegram/commands/general/HelpCommand.java
@@ -2,15 +2,15 @@ package ru.itmo.iad.photorecognize.telegram.commands.general;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
 import ru.itmo.iad.photorecognize.telegram.commands.AbsCommand;
 import ru.itmo.iad.photorecognize.telegram.response.StringResponse;
 
 @Service
 @Scope("prototype")
 public class HelpCommand extends AbsCommand {
-	
-	public HelpCommand() {}
+
+    public HelpCommand() {
+    }
 
 	@Override
 	public StringResponse execute() {
@@ -19,7 +19,7 @@ public class HelpCommand extends AbsCommand {
 		builder.append("Описание классов с примерами:");
 		builder.append(System.lineSeparator());
 		builder.append(System.lineSeparator());
-		builder.append("ссылки ещё нет");
+		builder.append("https://docs.google.com/document/d/1pG41a8pfhYZNnzaHU08J-A06_P2_QkFhOHgTluehetU");
 		
 		return new StringResponse(builder.toString());
 	}
--- src/main/java/ru/itmo/iad/photorecognize/telegram/commands/general/StartCommand.java
+++ src/main/java/ru/itmo/iad/photorecognize/telegram/commands/general/StartCommand.java
@@ -1,7 +1,6 @@
 package ru.itmo.iad.photorecognize.telegram.commands.general;
 
 import org.springframework.stereotype.Service;
-
 import ru.itmo.iad.photorecognize.telegram.commands.AbsCommand;
 import ru.itmo.iad.photorecognize.telegram.response.Response;
 import ru.itmo.iad.photorecognize.telegram.response.StringResponse;
@@ -9,17 +8,17 @@ import ru.itmo.iad.photorecognize.telegram.response.StringResponse;
 @Service
 public class StartCommand extends AbsCommand {
 
-	@Override
-	public Response<?> execute() {
-		
-		
-		StringBuilder builder = new StringBuilder();
-		builder.append("Велком ту бот:");
-		builder.append(System.lineSeparator());
-		builder.append(System.lineSeparator());
-		builder.append("ссылки ещё нет");
-		
-		return new StringResponse(builder.toString());
-	}
+    @Override
+    public Response<?> execute() {
+
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Велком ту бот:");
+        builder.append(System.lineSeparator());
+        builder.append(System.lineSeparator());
+        builder.append("https://docs.google.com/document/d/1pG41a8pfhYZNnzaHU08J-A06_P2_QkFhOHgTluehetU");
+
+        return new StringResponse(builder.toString());
+    }
 
 }
