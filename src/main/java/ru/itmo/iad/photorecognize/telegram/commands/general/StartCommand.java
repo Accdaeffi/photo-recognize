a8d4bdee2d35712bd8a647590a43a571e73a1884
iburakov <speedwatson@gmail.com>
2022-10-29 12:47:54 +0300

    Update start text

--- src/main/java/ru/itmo/iad/photorecognize/telegram/commands/general/StartCommand.java
+++ src/main/java/ru/itmo/iad/photorecognize/telegram/commands/general/StartCommand.java
@@ -13,10 +13,16 @@ public class StartCommand extends AbsCommand {
 
 
         StringBuilder builder = new StringBuilder();
-        builder.append("Велком ту бот:");
+        // TODO: refactor
+        builder.append("Привет! Это бот, с помощью которого ты можешь помочь нам разметить фотографии.\n\n" +
+            "Все просто: ты получаешь фотографию и выбираешь, какой класс больше всего подходит " +
+            "главному объекту на изображении или идее изображения в целом. " +
+            "Людей и животных на фотографиях можно игнорировать.\n\n"
+            + "Мы написали подробное описание классов - обращайся к нему, если что-то не понятно.");
         builder.append(System.lineSeparator());
         builder.append(System.lineSeparator());
-        builder.append("https://docs.google.com/document/d/1pG41a8pfhYZNnzaHU08J-A06_P2_QkFhOHgTluehetU");
+        builder.append("https://docs.google.com/document/d/1pG41a8pfhYZNnzaHU08J-A06_P2_QkFhOHgTluehetU"
+            + "\n\nЧтобы начать, отправь команду /next.");
 
         return new StringResponse(builder.toString());
     }
