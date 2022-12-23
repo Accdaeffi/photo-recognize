965b5012782a0e0642ae54366c4e105e833c2aa8
Irina Redkina <irisha132132@mail.ru>
2022-12-23 21:32:42 +0300

    add image checker

--- src/main/java/ru/itmo/iad/photorecognize/telegram/commands/photos/RecognizePhotoCommand.java
+++ src/main/java/ru/itmo/iad/photorecognize/telegram/commands/photos/RecognizePhotoCommand.java
@@ -14,6 +14,7 @@ import ru.itmo.iad.photorecognize.telegram.response.StringResponse;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -25,7 +26,7 @@ public class RecognizePhotoCommand extends AbsCommand {
     @Autowired
     UserService userService;
 
-    private User telegramUser;
+    private final User telegramUser;
     private final List<PhotoSize> photoSizes;
 
     public RecognizePhotoCommand(User telegramUser, List<PhotoSize> photoSizes) {
@@ -41,10 +42,49 @@ public class RecognizePhotoCommand extends AbsCommand {
         UserDao user = userService.getOrCreateUser(telegramUser);
 
         PhotoSize photo = photoSizes.stream().max(Comparator.comparing(PhotoSize::getFileSize)).orElse(null);
+
+        //TODO: добавила валидацию картинок, потом нужно будет раскомментировать
+        /*String result = checkUserImage(photo);
+        if (!result.equals("ok")) {
+            return new StringResponse(result);
+        }*/
+
         imageSaver.saveUserImage(user.getTelegramId(), photo);
 
         log.info("Photo saved, user updated!");
 
         return new StringResponse("Success!");
+    }
+
+    private String checkUserImage(PhotoSize photo) {
+        StringBuilder result = new StringBuilder();
+        Optional.ofNullable(photo).ifPresentOrElse(
+                (photoToCheck) -> {
+                    if (!checkSizes(photoToCheck) || !checkExtension(photoToCheck)
+                            || !checkSize(photoToCheck)) {
+                        result.append("User image has invalid size or extension " +
+                                "(max size is 1920*1080, image: png, jpg, jpeg)");
+                    } else {
+                        result.append("ok");
+                    }
+                },
+                () -> result.append("Empty image")
+        );
+        return result.toString();
+    }
+
+    private boolean checkSize(PhotoSize photo) {
+        int sizeInKb = photo.getFileSize() / 1024;
+        return sizeInKb >= 1 && sizeInKb / 1024 <= 50;
+    }
+
+    private boolean checkSizes(PhotoSize photo) {
+        return photo.getHeight() < 1080 && photo.getWidth() < 1920;
+    }
+
+    private boolean checkExtension(PhotoSize photo) {
+        return photo.getFilePath().endsWith(".jpg")
+                || photo.getFilePath().endsWith(".jpeg")
+                || photo.getFilePath().endsWith(".png");
     }
 }
