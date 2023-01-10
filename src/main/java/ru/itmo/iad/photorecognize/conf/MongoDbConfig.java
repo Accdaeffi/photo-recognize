dff798068313dfc491f2778a93dcac7be5f24378
Дмитрий Носков <noskoff2@yandex.ru>
2023-01-10 21:27:11 +0300

    removed unused mongo db work and restore config for saving images

--- src/main/java/ru/itmo/iad/photorecognize/conf/MongoDbConfig.java
+++ src/main/java/ru/itmo/iad/photorecognize/conf/MongoDbConfig.java
@@ -1,18 +1,26 @@
 package ru.itmo.iad.photorecognize.conf;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = { "ru.itmo.iad.photorecognize.domain" })
+@EnableMongoRepositories(basePackages = {"ru.itmo.iad.photorecognize.domain"})
 public class MongoDbConfig {
 
-	/*
-	 * @Value("${spring.data.mongodb.bucket}") String bucket;
-	 * 
-	 * @Bean public GridFsTemplate gridFsTemplate(MongoDatabaseFactory dbFactory,
-	 * MongoConverter converter) { return new GridFsTemplate(dbFactory, converter,
-	 * bucket); }
-	 */
+
+    @Value("${spring.data.mongodb.bucket}")
+    String bucket;
+
+    @Bean
+    public GridFsTemplate gridFsTemplate(MongoDatabaseFactory dbFactory,
+                                         MongoConverter converter) {
+        return new GridFsTemplate(dbFactory, converter, bucket);
+    }
+
 
 }
